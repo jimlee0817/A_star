ef538f6a0c843ad83494def6207d70fed30f7056
jimlee0817 <jimlee19980817@gapp.nthu.edu.tw>
2020-04-13 18:32:25 +0800

    second upload

--- src/A_Star_PathFinding.cpp
+++ src/A_Star_PathFinding.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <sstream>
 #include <vector>
+#include <stack>
 
 #define X_MAX 4.9
 #define X_MIN -5
@@ -35,10 +36,40 @@ struct Node
 	double theta;
 	double parent_x;
 	double parent_y;
-	double h = 0;
-	double g = 0;
-	double f = g + h;
+	double h;
+	double g;
+	double f;
 };
+
+
+int double2index(double x)
+{
+        int x_int;
+        double x_temp;
+        x_temp = (x+5)*10+0.5;
+        x_int = (int)x_temp;
+        return x_int;
+}
+double double2decimal1(double x)
+{
+        double x_decimal1;
+        double x_temp;
+        int x_int;
+        x_temp = (x*10)+0.5;
+        x_int = (int)x_temp;
+        x_decimal1 = x_int/10.0;
+        return x_decimal1;
+}
+double index2double(int x)
+{
+        double x_double;
+        int x_temp;
+        x_temp = x-50;
+        x_double = (double)x_temp;
+        x_double = x_double/10;
+        double2decimal1(x_double);
+        return x_double;
+}
 
 int cart2map(double x, double y)
 {
@@ -47,38 +78,18 @@ int cart2map(double x, double y)
 	return mapGrid[idx];
 }
 
-bool isValid(Node node)
+bool isValid(double x, double y)
 {
-	if(node.x > X_MAX || node.x < X_MIN || node.y > Y_MAX || node.y < Y_MIN)	return false;
+	if(x > X_MAX || x < X_MIN || y > Y_MAX || y < Y_MIN)	return false;
 	
-	if(cart2map(node.x, node.y) != 0)	return false;
-	
-	if(cart2map(node.x + 0.3, node.y) != 0)	return false;
-	if(cart2map(node.x, node.y+ 0.3) != 0)	return false;
-	if(cart2map(node.x - 0.3, node.y) != 0)	return false;
-	if(cart2map(node.x, node.y - 0.3) != 0)	return false;
-	if(cart2map(node.x + 0.3, node.y + 0.3) != 0)	return false;
-	if(cart2map(node.x + 0.3, node.y - 0.3) != 0)	return false;
-	if(cart2map(node.x - 0.3, node.y - 0.3) != 0)	return false;
-	if(cart2map(node.x - 0.3, node.y + 0.3) != 0)	return false;
-	
-	if(cart2map(node.x + 0.2, node.y) != 0)	return false;
-	if(cart2map(node.x, node.y+ 0.2) != 0)	return false;
-	if(cart2map(node.x - 0.2, node.y) != 0)	return false;
-	if(cart2map(node.x, node.y - 0.2) != 0)	return false;
-	if(cart2map(node.x + 0.2, node.y + 0.2) != 0)	return false;
-	if(cart2map(node.x + 0.2, node.y - 0.2) != 0)	return false;
-	if(cart2map(node.x - 0.2, node.y - 0.2) != 0)	return false;
-	if(cart2map(node.x - 0.2, node.y + 0.2) != 0)	return false;
-	
-	if(cart2map(node.x + 0.1, node.y) != 0)	return false;
-	if(cart2map(node.x, node.y+ 0.1) != 0)	return false;
-	if(cart2map(node.x - 0.1, node.y) != 0)	return false;
-	if(cart2map(node.x, node.y - 0.1) != 0)	return false;
-	if(cart2map(node.x + 0.1, node.y + 0.1) != 0)	return false;
-	if(cart2map(node.x + 0.1, node.y - 0.1) != 0)	return false;
-	if(cart2map(node.x - 0.1, node.y - 0.1) != 0)	return false;
-	if(cart2map(node.x - 0.1, node.y + 0.1) != 0)	return false;
+	for(double newX = -0.2; newX <= 0.2; newX = newX + 0.1)
+	{
+		for(double newY = -0.2; newY <= 0.2; newY = newY + 0.1)
+		{
+			if(cart2map(x + newX, y + newY) != 0) return false;
+		}
+		
+	}
 	
 	
 	return true;
@@ -90,9 +101,9 @@ bool isValid(Node node)
 	//-	  -	  -
 }
 
-double calcHeuristic(Node node1, Node goal)
+double calcHeuristic(double x, double y, Node goal)
 {
-	return sqrt((node1.x - goal.x) * (node1.x - goal.x) + (node1.y - goal.y) * (node1.y - goal.y));
+	return sqrt((x - goal.x) * (x - goal.x) + (y - goal.y) * (y - goal.y));
 }
 
 double constrainAngle(double x)
@@ -132,262 +143,175 @@ void map_Callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 	flagMap = true;
 }
 
-bool isRepeat(Node successor, Node node) // check if the successors go back or not
+bool isGoal(double x, double y, Node goal) // check goal state
 {
-	if(successor.x == node.parent_x && successor.y == node.parent_y)
+	if(abs(x - goal.x) < 0.0001 && abs(y - goal.y) < 0.0001)
 	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
-bool isGoal(Node node, Node goal) // check goal state
-{
-	if(abs(node.x - goal.x) < 0.0001 && abs(node.y - goal.y) < 0.0001)
-	{
-		ROS_INFO("Is Goal");
+		ROS_INFO("Is Goal (%f, %f)", goal.x, goal.y);
 		return true;
 	}
 	else
 	{	
-		ROS_INFO("Not Goal yet"); 
+		ROS_INFO("Not Goal (%f, %f) yet, now at (%f, %f)", goal.x, goal.y, x, y); 
 		return false;
 	}
 }
 
-vector<Node> getSuccessors(Node node, Node goal) 
-{
-	vector<Node> successors;
-	Node newNode;
-	//ROS_INFO("In getSuccessors: node's parent: (%f, %f), node: (%f, %f)", node.parent_x, node.parent_y, node.x, node.y);
-	double factor = calcHeuristic(node, goal);
-	double inc;
-	if(factor > 4) inc = 0.9;
-	else if(factor <= 4 && factor > 2.5) inc = 0.5;
-	else if(factor <= 2.5 && factor > 1) inc = 0.3;
-	else if(factor <= 1 && factor > 0.5) inc = 0.2;
-	else inc = 0.1; 
-	
-	ROS_INFO("The increment now is %f", inc);
-	newNode.x = node.x + inc; 	newNode.y = node.y; // start from EAST
-	if(isValid(newNode) && !isRepeat(newNode, node))
-	{
-		newNode.g = node.g + inc;
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode);
 
-		//ROS_INFO("The first successor is (%f, %f) from parent node (%f, %f)", node.successors[0]->x, node.successors[0]->y, node.x, node.y);
-	} 
-	newNode.x = node.x - inc; 	newNode.y = node.y; // WEST
-	if(isValid(newNode) && !isRepeat(newNode, node))
-	{	
-		newNode.g = node.g + inc;
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode); 
-	}
-	newNode.x = node.x; 	newNode.y = node.y + inc; // NORTH
-	if(isValid(newNode) && !isRepeat(newNode, node))
-	{	
-		newNode.g = node.g + inc;
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode); 
-	} 
-	newNode.x = node.x; 	newNode.y = node.y - inc; // SOUTH
-	if(isValid(newNode) && !isRepeat(newNode, node))
-	{	
-		newNode.g = node.g + inc;
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode);
-	}
-	newNode.x = node.x + inc; 	newNode.y = node.y + inc; // NORTH-EAST
-	if(isValid(newNode) && !isRepeat(newNode, node))
-	{	
-		newNode.g = node.g + sqrt(inc*inc + inc*inc);
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode);
-	}
-	newNode.x = node.x - inc; 	newNode.y = node.y + inc; // NORTH-WEST
-	if(isValid(newNode) && !isRepeat(newNode, node)) 
-	{	
-		newNode.g = node.g + sqrt(inc*inc + inc*inc);
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode);
-	}
-	newNode.x = node.x + inc; 	newNode.y = node.y - inc; // SOUTH-EAST
-	if(isValid(newNode) && !isRepeat(newNode, node)) 
-	{	
-		newNode.g = node.g + sqrt(inc*inc + inc*inc);
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;
-		successors.push_back(newNode); 
-	}
-	newNode.x = node.x - inc; 	newNode.y = node.y - inc; // SOUTH-WEST
-	if(isValid(newNode) && !isRepeat(newNode, node)) 
-	{	
-		newNode.g = node.g + sqrt(inc*inc + inc*inc);
-		newNode.h = calcHeuristic(newNode, goal);
-		newNode.f = newNode.g + newNode.h;	
-		newNode.parent_x = node.x;
-		newNode.parent_y = node.y;	
-		successors.push_back(newNode); 
-	}
+vector<Node> makePath(Node map[100][100], Node goal)
+{
+	double x = goal.x;
+	double y = goal.y;
 	
+	vector<Node> path;
+	vector<Node> outputPath;
+	int idx_x = double2index(x);
+	int idx_y = double2index(y);
 	
-	return successors;
+	while(!(map[idx_x][idx_y].parent_x == x && map[idx_x][idx_y].parent_y == y) && map[idx_x][idx_y].parent_x != -6 && map[idx_x][idx_y].parent_y != -6)
+	{
+		idx_x = double2index(x);
+		idx_y = double2index(y);
+		path.push_back(map[idx_x][idx_y]);
+		x = map[idx_x][idx_y].parent_x;
+		y = map[idx_x][idx_y].parent_y;
+		idx_x = double2index(x);
+		idx_y = double2index(y);
+	}
+	idx_x = double2index(x);
+	idx_y = double2index(y);
+	path.push_back(map[idx_x][idx_y]);
+	
+	//outputPath.resize(path.size());
+	for(int i = path.size(); i >= 0; i--)
+	{
+		outputPath.push_back(path[i]);
+	}
+	return outputPath;
 }
 
-vector<Node> A_star(Node initNode, Node goalNode)
+vector<Node> A_star(Node init, Node goal)
 {
-
-	vector<vector<Node> > reachedList;
-	Node node;
-	node = initNode;
-	if(!isValid(initNode) || !isValid(goalNode))
+	vector<Node> empty;
+	if(isValid(goal.x, goal.y) == false)
 	{
-		ROS_INFO("Init: (%f, %f), Goal: (%f, %f)", initNode.x, initNode.y, goalNode.x, goalNode.y);
-		ROS_INFO("GOAL or INITIAL Point is not valid");
-		return {};
+		ROS_INFO("GOAL IST A WALL FUCK!!!!");
+		return empty;
+	}
+	if(isGoal(init.x, init.y, goal))
+	{
+		ROS_INFO("YOU ARE AT GOAL!!!");
+		return empty;
 	}
 	
-	vector<Node> reachedInit;
-	reachedInit.push_back(node);
-	//bool islstRun = true;
-	int minIdx = 0;
+	bool closedList[100][100];
 	
-	while(!isGoal(node, goalNode)) // Check if it's goal state or not
+	int idx_x;
+	int idx_y;
+	//INITIALIZE THE WHOLE MAP
+	Node allMap[100][100];
+	for(double x = -5; x <= 4.9; x = x + 0.1 )
 	{
-		ROS_INFO("Now we are at node (%f, %f)", node.x, node.y);
-		
-		vector<Node> successors = getSuccessors(node, goalNode);
-		ROS_INFO("We have successors:");
-		bool canUse = false;
-		for(int i = 0; i < successors.size(); i++)
+		for(double y = -5; y <= 4.9; y = y + 0.1)
 		{
-			ROS_INFO("(%f, %f), g = %f", successors[i].x, successors[i].y, successors[i].h);
-			for(int j = 0; j < reachedInit.size(); j++)
-			{
-				if(!(abs(successors[i].x - reachedInit[j].x) < 0.0001 && abs(successors[i].y - reachedInit[j].y) < 0.0001))	canUse = true; // 查看successors是否往回走
-				else canUse = false;
-			}
+			idx_x = double2index(x);
+			idx_y = double2index(y);
+			allMap[idx_x][idx_y].f = FLT_MAX;
+			allMap[idx_x][idx_y].g = FLT_MAX;
+			allMap[idx_x][idx_y].h = FLT_MAX;
+			allMap[idx_x][idx_y].parent_x = -6;
+			allMap[idx_x][idx_y].parent_y = -6;
+			allMap[idx_x][idx_y].x = x;
+			allMap[idx_x][idx_y].y = y;
 			
-			/*
-			if(i == 0 && !islstRun)
+			closedList[idx_x][idx_y] = false;
+		}
+	}
+	
+	double x = init.x;
+	double y = init.y;
+	idx_x = double2index(x);
+	idx_y = double2index(y);
+	allMap[idx_x][idx_y].f = 0.0;
+	allMap[idx_x][idx_y].g = 0.0;
+	allMap[idx_x][idx_y].h = 0.0;
+	allMap[idx_x][idx_y].parent_x = x;
+	allMap[idx_x][idx_y].parent_y = y;
+	
+	vector<Node> openList;
+	openList.push_back(allMap[idx_x][idx_y]); // push the inital point into the open list
+	bool goalFound = false;
+	
+	while(!openList.empty() && openList.size() < 10000)
+	{
+		Node node;
+		do
+		{
+			double temp = FLT_MAX;
+			vector<Node>::iterator itNode;
+			for(vector<Node>::iterator it = openList.begin(); it != openList.end(); it++)
 			{
-				reachedList[minIdx].push_back(successors[i]);
-			}
-			*/
-			
-			if(canUse)
-			{
-				vector<Node> reached;
-				reached = reachedInit;
-				reached.push_back(successors[i]); 
-				int count = 0;
-				
-				for(int j = 0; j < reachedList.size(); j++)
+				Node n = *it;
+				if(n.f < temp)
 				{
-					for(int k = 0; k < reachedList[j].size(); k++)
+					temp = n.f;
+					itNode = it;
+				}
+			}
+			node = *itNode;
+			openList.erase(itNode);
+		} while(!isValid(node.x, node.y));
+		
+		x = node.x;
+		y = node.y;
+		
+		closedList[idx_x][idx_y] = true;
+		
+		for(double newX = -0.1; newX <= 0.1; newX = newX + 0.1)
+		{
+			for(double newY = -0.1; newY <= 0.1; newY = newY + 0.1)
+			{
+				double newF, newG, newH;
+				idx_x = double2index(x + newX);
+				idx_y = double2index(y + newY);
+				
+				if(isValid(x + newX, y + newY))
+				{
+					if(isGoal(x + newX, y + newY, goal))
 					{
-						if(abs(reached[k].x - reachedList[j][k].x) < 0.0001 && abs(reached[k].y - reachedList[j][k].y) < 0.0001) count++;
+						allMap[idx_x][idx_y].parent_x = x;
+						allMap[idx_x][idx_y].parent_y = y;
+						goalFound = true;
+						ROS_INFO("TESTING (%f, %f)", x, y);
+						ROS_INFO("TESTING PARENT (%f, %f)", allMap[idx_x][idx_y].parent_x, allMap[idx_x][idx_y].parent_y);
+						return makePath(allMap, goal);
+					}
+					else if(closedList[idx_x][idx_y] == false)
+					{
+						newG = node.g + sqrt(newX*newX + newY*newY);
+						newH = calcHeuristic(x + newX, y + newY, goal);
+						newF = newG + newH;
+						// check if this path better than the other
+						if(allMap[idx_x][idx_y].f == FLT_MAX || allMap[idx_x][idx_y].f > newF) // don't go back
+						{
+							allMap[idx_x][idx_y].f = newF;
+							allMap[idx_x][idx_y].g = newG;
+							allMap[idx_x][idx_y].h = newH;
+							allMap[idx_x][idx_y].parent_x = x;
+							allMap[idx_x][idx_y].parent_y = y;
+							openList.push_back(allMap[idx_x][idx_y]);
+						}
 					}
 				}
-				if(!(count == reached.size()))
-				{
-					
-					reachedList.push_back(reached);
-				}
-				else ROS_INFO("YOU GOT REPEATED PATH!!!!!!!!!!!!!!!!!!!");
 			}
 		}
-		
-		// Testing
-		/*
-		for(int temp = 0; temp < reachedList.size(); temp++)
-		{
-			ROS_INFO("Path %d is:", temp);
-			for(int temp2 = 0; temp2 < reachedList[temp].size(); temp2++)
-			{
-				ROS_INFO("(%f, %f)", reachedList[temp][temp2].x, reachedList[temp][temp2].y);
-			}
-		}
-		*/
-		//****************
-		
-		double min = 10000000;
-		
-		
-		for(int i = 0; i < reachedList.size(); i++)
-		{	
-			if(reachedList[i][reachedList[i].size() - 1].f < min)
-			{
-				min = reachedList[i][reachedList[i].size() -1].f;
-				minIdx = i;
-			}
-			/*
-			if(reachedList[i][reachedList[i].size() -1].h == 0)
-			{
-				//min = reachedList[i][reachedList[i].size() -1].f;
-				minIdx = i;
-				i = reachedList.size(); // out the for loop
-				ROS_INFO("Find GOAL!!!!!!!!!!!!");
-			}
-			else
-			{
-				if(reachedList[i][reachedList[i].size() - 1].f < min)
-				{
-					min = reachedList[i][reachedList[i].size() -1].f;
-					minIdx = i;
-				}
-			}
-			*/
-		}
-		
-		reachedInit = reachedList[minIdx];
-		node = reachedList[minIdx][reachedList[minIdx].size() -1];
-		vector<vector<Node> > newReachedList;
-		for(int i = 0; i < reachedList.size(); i++)
-		{
-			if(i != minIdx)
-			{
-				newReachedList.push_back(reachedList[i]);
-			}
-		}
-		
-		reachedList = newReachedList;
-		//ROS_INFO("Size if reachedList is %d", reachedList.size());
-		//reachedList.erase(reachedList.begin() + minIdx);
-		//ROS_INFO("After Size if reachedList is %d", reachedList.size());
-		ROS_INFO("The path is: f = %f, List n = %d", reachedInit[reachedInit.size() - 1].f, reachedList.size());
-		for(int i = 0; i < reachedInit.size(); i++)
-		{
-			ROS_INFO("(%f, %f)", reachedInit[i].x, reachedInit[i].y);
-		}
-		//islstRun = false;
 	}
-	return reachedInit;	
+	if(goalFound == false)
+	{
+		ROS_INFO("CAN'T FIND GOAL");
+		return empty;
+	}
+	
 }
 
 int main(int argc, char **argv)
@@ -401,6 +325,7 @@ int main(int argc, char **argv)
 	ros::Rate loop_rate(10);
 
 	Node nowPos;
+	
 	Node goal1;	goal1.x = 3;	goal1.y = 4;
 	Node goal2;	goal2.x = 3;	goal2.y = -2;
 	Node goal3;	goal3.x = -4;	goal3.y = -3;
@@ -426,23 +351,25 @@ int main(int argc, char **argv)
 		
 			nowPos.x = round(10 * x) / 10.0;
 			nowPos.y = round(10 * y) / 10.0;
-			nowPos.h = 0; nowPos.g = 0;
+			
+			//nowPos.h = 0; nowPos.g = 0;
 		
 			nowPos.theta = theta;
-			nowPos.parent_x = -9999; nowPos.parent_y = -9999;
+			//nowPos.parent_x = -9999; nowPos.parent_y = -9999;
 		
 			ROS_INFO ("The Car is initial at (%f, %f, %f)", nowPos.x, nowPos.y, nowPos.theta);
 			//ROS_INFO ("The Parant Node of the car position now is (%f, %f, %f)", nowPos.parent->x, nowPos.parent->y, nowPos.parent->theta);
 			//getSuccessors(nowPos, goal1);
 			path1 = A_star(nowPos, goal1);
-			path2 = A_star(path1[path1.size() - 1], goal2);
-			path3 = A_star(path2[path2.size() - 1], goal3);
-			path4 = A_star(path3[path3.size() - 1], goal4);
+			path2 = A_star(goal1, goal2);
+			path3 = A_star(goal2, goal3);
+			path4 = A_star(goal3, goal4);
 			ROS_INFO ("Path1: ");
 			for(int i = 0; i < path1.size(); i++)
 			{
 				ROS_INFO ("(%f, %f)", path1[i].x, path1[i].y);
 			}
+			
 			ROS_INFO ("Path2: ");
 			for(int i = 0; i < path2.size(); i++)
 			{
@@ -500,14 +427,11 @@ int main(int argc, char **argv)
 		    ROS_INFO ("Now Goal: (%f, %f, %f)", goalStep.x, goalStep.y, goalStep.theta * 180 / PI);
 		    if(goal_idx != PATH.size() - 1)
 		    {
-		    	if(abs(goalStep.parent_x - goalStep.x) == 0)
-		    		Tolerance = abs(goalStep.parent_y - goalStep.y) - 0.08;
-		    	else
-		    		Tolerance = abs(goalStep.parent_x - goalStep.x) - 0.08;
+		    	Tolerance = 0.2;
 		    }
 		    else
 		    {
-		    	Tolerance = 0.01;
+		    	Tolerance = 0.01; 
 		    }
 		    
 		    if (rho < Tolerance)// Stop when close to the goal
@@ -547,6 +471,8 @@ int main(int argc, char **argv)
 		       w = alpha * K_alpha + beta * K_beta;
 		       if(v > 0.5){	v = 0.5;}
 		       if(w > 1.0){	w = 1.0;}
+		       ROS_INFO ("velocity: %f", v);
+		       ROS_INFO ("angular velocity: %f", w);
 		       vel_msg.linear.x = v;
 		       vel_msg.angular.z = w;
 		       velocity_pub.publish(vel_msg); 
